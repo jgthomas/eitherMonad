297e463f4bbb9e1a2bb4a219cd08827f8150ccf8
James Thomas <codeactor@protonmail.com>
2020-11-02 12:33:28 +0000

    Add comments

--- src/EitherM.hs
+++ src/EitherM.hs
@@ -2,10 +2,10 @@ module EitherM where
 
 import Control.Applicative (Applicative)
 import Control.Monad (ap, liftM)
-import Control.Monad.Fail as Fail
+import Control.Monad.Fail (MonadFail)
 
 newtype EitherM a = EitherM (Either String a)
-    deriving (Show)
+  deriving (Show)
 
 instance Functor EitherM where
   fmap = liftM
@@ -22,5 +22,9 @@ instance Monad EitherM where
       Left msg -> EitherM (Left msg)
       Right a -> k a
 
-instance Fail.MonadFail EitherM where
+-- fail has been removed from the Monad
+-- class as it didn't make much sense for
+-- lots of Monads, so now has to be defined
+-- separately for those Monads that require it
+instance MonadFail EitherM where
   fail msg = EitherM (Left msg)
